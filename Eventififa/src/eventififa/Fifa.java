67dbeb53561ac33493ae9d5389534f43998ebe09
PC19 <PC19@DESKTOP-JM1S1P0.fritz.box>
2018-07-18 17:04:03 +0200

    Cancello la stampa

---
+++ Eventififa/src/eventififa/Fifa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventififa;
+
+/**
+ *
+ * @author PC19
+ */
+public class Fifa {
+    private String presidente;
+   
+    private String annoFond;
+
+    public String getPresidente() {
+        return presidente;
+    }
+
+    public void setPresidente(String presidente) {
+        this.presidente = presidente;
+    }
+
+       public String getAnnoFond() {
+        return annoFond;
+    }
+
+    public void setAnnoFond(String annoFond) {
+        this.annoFond = annoFond;
+    }
+    
+    
+    
+    
+    
+    
+}
