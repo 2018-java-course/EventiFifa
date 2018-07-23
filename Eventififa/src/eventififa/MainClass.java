a11509c7644fe006e3a0e45fb72f9b9de9412fb4
PC22 <PC22@DESKTOP-JBEI027.fritz.box>
2018-07-23 16:23:55 +0200

    Populate

--- Eventififa/src/eventififa/MainClass.java
+++ Eventififa/src/eventififa/MainClass.java
@@ -5,48 +5,25 @@
  */
 package eventififa;
 
+import calendar.api.CalendarEvent;
+import java.util.ArrayList;
+
 
 public class MainClass {
     public static void main(String[] args) {
     
-     Torneo [] international = new Torneo [10];
-     Squadra [] team = new Squadra [10]; //creo la struttura array
+     Torneo [] international;
+     international = new Torneo [10];
+  
      
-     team[0]=new Squadra(); // creo i campi dell'array
-     team[0].setNome("Argentina");
-     team[0].setPalmares("3 mondiali");
-     team[0].setContinentenativo("America del sud");
+    ArrayList<CalendarEvent> myArrayList = new ArrayList<>();
+     Populator populate = new Populator();
      
-     team[1]=new Squadra();
-     team[1].setNome("francia");
-     team[1].setPalmares("2 mondiali");
-     team[1].setContinentenativo("Europa");
+     populate.populate(myArrayList);
      
-     team[2]=new Squadra();
-     team[2].setNome("russia");
-     team[2].setPalmares("0 mondiali");
-     team[2].setContinentenativo("Europa");
-     
-     team[3]=new Squadra();
-     team[3].setNome("spagna");
-     team[3].setPalmares("1 mondiali");
-     team[3].setContinentenativo("Europa");
-     
-     team[4]=new Squadra();
-     team[4].setNome("italia");
-     team[4].setPalmares("4 mondiali");
-     team[4].setContinentenativo("Europa");
-     
-     international[0]= new Torneo("Mondiale","Russia","10 Giugno","9 luglio",10,team);
-     international[1]= new Torneo("europeo","Francia","16 Giugno 2020","7 Luglio 2020",16,team);    
-     
-     System.out.println("Torneo:"+ international[0].getNomeTorneo());
-     System.out.println("Organizzato dal presiudente FIFA:"+ international[0].presidente);
-     System.out.println("Il torneo si è svolto in: " + international[0].getSedeTorneo());
-     System.out.println("Data finale:" + international[0].getDataFine());
-     System.out.println("Hai vinto: " + international[0].premio);
-     
-     System.out.println(international[0].getNomeTorneo());
-    }
+      myArrayList.forEach((calendarEvent) -> {
+            System.out.println(((Torneo) calendarEvent).getNomeTorneo());
+      });
+}
     
 }
